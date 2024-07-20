0a4c11f00bbfdf61f1dd0ab63a9e1671b6311986
nasir-buddy <syedali172000@gmail.com>
2024-07-21 01:48:45 +0530

    fix warning for class in tailwind css and implemented no image

--- src/Components/template/Sidenav.jsx
+++ src/Components/template/Sidenav.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Sidenav = () => {
+
+
+
     return (
         // Header Section of nav
         <div className='w-[20%] h-full  border-r-2 border-zinc-400 p-10'>
             <h1 className='text-2xl text-white font-bold '>
-                <i class="text-[#6556CD] ri-tv-fill mr-2"></i>
+                <i className="text-[#6556CD] ri-tv-fill mr-2"></i>
                 <span>SCSDB</span>
             </h1>
             {/* Nav section */}
@@ -19,13 +22,13 @@ const Sidenav = () => {
                 <i className='mr-2 ri-bard-fill'></i>
                 Popular</Link>
                 <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
-                <i class="mr-2 ri-movie-2-line"></i>
+                <i className="mr-2 ri-movie-2-line"></i>
                 Movies</Link>
                 <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
-                <i class="mr-2 ri-tv-2-fill"></i>
+                <i className="mr-2 ri-tv-2-fill"></i>
                 Tv Shows</Link>
                 <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
-                <i class="mr-2 ri-group-line"></i>
+                <i className="mr-2 ri-group-line"></i>
                 People</Link>
             </nav>
 
@@ -34,10 +37,10 @@ const Sidenav = () => {
             <nav className='flex flex-col text-zinc-400 text-[1.2rem]'>
                 <h1 className='text-white font-semibold mt-10 mb-5'>Website Information</h1>
                 <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
-                <i class="mr-2 ri-info-i"></i>
-                About</Link>
+                <i className="mr-2 ri-info-i"></i>
+                AboutUs </Link>
                 <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
-                <i class="mr-2 ri-contacts-line"></i>
+                <i className="mr-2 ri-contacts-line"></i>
                 Contact</Link>
             </nav>
 
