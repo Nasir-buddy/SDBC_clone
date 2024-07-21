9d4a22aa91f76098a38182427d13d5f5d2db8c91
nasir-buddy <syedali172000@gmail.com>
2024-07-22 03:23:58 +0530

    fix warnings in dropdown

--- src/Components/template/Dropdown.jsx
+++ src/Components/template/Dropdown.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Dropdown = ({ title, options }) => {
     return (
-        <div class="w-64">
-    <select id="dropdown" class="block w-full px-4 py-2 text-white bg-zinc-700 border border-none rounded-md shadow-sm focus:outline-none focus:ring-2 ">
+        <div className="w-64">
+    <select id="dropdown" className="block w-full px-4 py-2 text-white bg-zinc-700 border border-none rounded-md shadow-sm focus:outline-none focus:ring-2 ">
       <option  value="" disabled>{title}</option>
         {options.map((item, index)=>(
-            <option  value={0}>{item.toUpperCase()}</option>
+            <option key={index} value={0}>{item.toUpperCase()}</option>
         ))}
     </select>
   </div>
