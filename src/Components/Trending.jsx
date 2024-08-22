52119673e4c0cea0b64d6decbc2bcc146c46c69c
nasir-buddy <syedali172000@gmail.com>
2024-08-22 15:21:33 +0530

    Implement the infinite scroll functionality

--- src/Components/Trending.jsx
+++ src/Components/Trending.jsx
@@ -5,45 +5,52 @@ import Dropdown from './template/Dropdown';
 import axios from '../utils/axios';
 import Cards from './template/Cards';
 import Loading from './template/Loading';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Trending = () => {
-    
-    const navigate = useNavigate();
-    const [category, setCategory] = useState('all');
-    const [duration, setDuration] = useState('day');
-    const [trending, setTrending] = useState(null);
 
+    const navigate = useNavigate();
+    const [category, setCategory] = useState("all");
+    const [duration, setDuration] = useState("day");
+    const [trending, setTrending] = useState([]);
+    const [page, setpage] = useState(1);
     const getTrending = async () => {
         try {
             const { data } = await axios.get(`/trending/${category}/${duration}`);
-            setTrending(data.results);
+            // setTrending(data.results);
+            setTrending((prev) => [...prev, ...data.results]);
+            setpage(page + 1);
+            console.log(data );
+            
 
         } catch (error) {
             console.log(error);
         }
     }
-    console.log(trending);
+
     
-    useEffect(()=>{
+
+    useEffect(() => {
         getTrending();
+    }, [duration, category])
 
-    }, [category, duration])
-
-    return trending ? (
-        <div className='w-screen h-screen px-[2%]'>
+    return trending.length > 0 ? (
+        <div className='w-screen h-screen'>
             <div className='w-full flex items-center'>
                 <h1 className='text-2xl text-zinc-400 font-semibold'>
-                <i
-                onClick={()=>{navigate(-1)}}
-                 class="hover:text-[#6556CD] ri-arrow-left-line mr-2"></i>
+                    <i
+                        onClick={() => { navigate(-1) }}
+                        className="hover:text-[#6556CD] ri-arrow-left-line mr-2"></i>
                     Trending</h1>
-                    <Topnav />
-                    <Dropdown title='category'  options={["movie", "tv", "all"]} func={(event)=>setCategory(event.target.value)}/>
-                    <div className='w-[2%]'></div>
-                    <Dropdown  title="duration" options={["week", "day"]} func={(event)=>{(event)=>setDuration(event.target.value)}}/>
+                <Topnav />
+                <Dropdown title='category' options={["movie", "tv", "all"]} func={(event) => setCategory(event.target.value)} />
+                <div className='w-[2%]'></div>
+                <Dropdown title="duration" options={["week", "day"]} func={(event) => setDuration(event.target.value)} />
             </div>
 
-        <Cards data={trending} />
+            <InfiniteScroll dataLength={trending.length} next={getTrending} hasMore={true} loader={<h1>Loading</h1>}>
+                <Cards data={trending} title={category} />
+            </InfiniteScroll>
 
         </div>
     ) : <Loading />
