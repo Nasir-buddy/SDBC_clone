07ae0c3b11cb163d887aba0017e06c578684f4e0
nasir-buddy <syedali172000@gmail.com>
2024-08-22 16:48:51 +0530

    route to trending and popular

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Components/Home'
+import Trending from './Components/Trending'
+import Popular from './Components/Popular'
 
 const App = () => {
   return (
     <div className='bg-[#1F1E24] w-screen h-screen flex'>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/trending' element={<Trending />} />
+        <Route path='/popular' element={<Popular />}></Route>
       </Routes>
     </div>
   )
