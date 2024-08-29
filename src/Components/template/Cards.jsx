b616f21dd2c5abe32b18c4c53b9668ef74b6b653
nasir-buddy <syedali172000@gmail.com>
2024-08-29 17:06:45 +0530

    add link to the card

--- src/Components/template/Cards.jsx
+++ src/Components/template/Cards.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Cards = ({ data, title }) => {
-  return (
+    return (
     <div className='flex flex-wrap w-full h-full px-[5%] py-12 bg-[#1F1E24]'>
       {data.map((card, index) =>
-        <Link key={index}
+        <Link
+          to={`/${card.media_type || title}/details/${card.id}`} 
+          key={index}
           className='relative w-[17vw] mb-10 mr-[1%] hover:translate-x-1 hover:scale-110 transition ease-in-out delay-100 '
         >
           <img className='h-[40vh] rounded-md shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)] hover:shadow-[8px_17px_38px_2px_rgba(101,86,205,1)]'
