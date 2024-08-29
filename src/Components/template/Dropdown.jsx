01edb45e3b753b05c0a69ec9eb08f68bdc3dab59
nasir-buddy <syedali172000@gmail.com>
2024-08-29 17:07:39 +0530

    upadted dropbox

--- src/Components/template/Dropdown.jsx
+++ src/Components/template/Dropdown.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const Dropdown = ({ title, options }) => {
+const Dropdown = ({ title, options, func }) => {
     return (
         <div className="w-64">
-    <select id="dropdown" className="block w-full px-4 py-2 text-white bg-zinc-700 border border-none rounded-md shadow-sm focus:outline-none focus:ring-2 ">
+    <select id="dropdown"
+    onChange={func}
+     className="block w-full px-4 py-2 text-white bg-zinc-700 border border-none rounded-md shadow-sm focus:outline-none focus:ring-2 ">
       <option  value="" disabled>{title}</option>
         {options.map((item, index)=>(
-            <option key={index} value={0}>{item.toUpperCase()}</option>
+            <option key={index} value={item}>{item.toUpperCase()}</option>
         ))}
     </select>
   </div>
