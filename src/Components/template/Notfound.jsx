41006900e575b464384319e16df5bf5a53d3650e
nasir-buddy <syedali172000@gmail.com>
2024-09-08 01:05:16 +0530

    implement the not found component

---
+++ src/Components/template/Notfound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notfound = () => {
+    return (
+        <div>
+            <img src="https://kfg6bckb.media.zestyio.com/yalantis-interactive-404.gif" />
+        </div>
+    )
+}
+
+export default Notfound 
