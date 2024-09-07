8f153830807a8bc2f08961c26ca8ffb127d84873
nasir-buddy <syedali172000@gmail.com>
2024-09-08 01:04:32 +0530

    implement the trailer component

--- src/Components/template/Trailer.jsx
+++ src/Components/template/Trailer.jsx
@@ -2,26 +2,29 @@ import React from 'react'
 import ReactPlayer from 'react-player'
 import { useSelector } from 'react-redux';
 import { Link, useLocation, useNavigate } from 'react-router-dom'
+import Notfound from './Notfound';
+
 
 const Trailer = () => {
     const navigate = useNavigate();
     const { pathname } = useLocation();
-
-    const category = pathname.includes("movie") ? 'movie' : 'tv';
+    const category = pathname.includes("movie") ? "movie" : "tv";
     const ytvideo = useSelector((state) => state[category].info.videos);
-
+    console.log("ytvideos" , ytvideo);
+    
     return (
         <div className='bg-[rgba(0,0,0,.7)] w-screen h-screen flex items-center justify-center top-0 left-0 z-10 absolute text-5xl'>
             <Link
                 onClick={() => { navigate(-1) }}
                 className="hover:text-[#6556CD] ri-close-large-line absolute top-[4%] left-[8%]"></Link>
-            <ReactPlayer
+
+            {ytvideo ? <ReactPlayer
                 height={800}
                 width={1500}
-                url={`https://www.youtube.com/watch?v=${ytvideo.key}`} />
+                url={`https://www.youtube.com/watch?v=${ytvideo.key}`} /> : <Notfound />}
 
         </div>
-    )
+    ) 
 }
 
 export default Trailer      
