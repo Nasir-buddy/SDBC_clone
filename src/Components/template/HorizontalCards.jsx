4b6f28aa20b5dc9f260a6ff3df321c347dc2e0f6
nasir-buddy <syedali172000@gmail.com>
2024-09-07 22:56:43 +0530

    modified horizontal cards components

--- src/Components/template/HorizontalCards.jsx
+++ src/Components/template/HorizontalCards.jsx
@@ -6,21 +6,21 @@ const HorizontalCards = ({ data }) => {
     console.log(data);
     
     return (
-            <div className='flex w-[100%] overflow-y-hidden p-4'>
+            <div className='flex w-[100%] max-h-[38%] overflow-y-hidden p-4'>
                 {data.map((d, i) => (<Link
                 to={`/${d.media_type}/details/${d.id}`}
                 key={i} className='min-w-[17%] mr-5 rounded p-2 bg-zinc-900 
                 transition ease-in-out delay-100 hover:translate-x-1 hover:scale-110 hover:bg-[#6556CD] duration-300'> 
                     <img
-                        className='w-[full] h-[40%] object-cover rounded transition ease-in-out '
+                        className='rounded transition ease-in-out '
                         src={`https://image.tmdb.org/t/p/original/${d.backdrop_path || d.profile_path}`} alt="" />
 
-                    <div className='text-white p-2 h-[60%]'>
+                    <div className='text-white p-2 h-[55%] overflow-y-auto'>
                         <h1 className='text-xl font-bold mb-3 mt-3'>
                             {d.name || d.title || d.original_name || d.original_title}
                         </h1>
                         <p className='mt-3 mb-5'>
-                            {d.overview.slice(0, 50)}
+                            {d.overview.slice(0, 100)}
                         </p>
                     </div>
                 </Link>))}
