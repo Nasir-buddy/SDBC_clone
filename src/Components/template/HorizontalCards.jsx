390a698f34f5039558e7a8d03c37d510dca8902d
nasir-buddy <syedali172000@gmail.com>
2024-09-09 20:08:02 +0530

    modify animation of card

--- src/Components/template/HorizontalCards.jsx
+++ src/Components/template/HorizontalCards.jsx
@@ -11,17 +11,17 @@ const HorizontalCards = ({ data }) => {
             <Link
               to={`/${d.media_type}/details/${d.id}`}
               key={i}
-              className="flex-shrink-0 w-72 sm:w-72 bg-zinc-900 rounded-lg overflow-hidden shadow-lg 
-                     transition-transform duration-300 ease-in-out transform  
+              className="flex-shrink-0 p-1 hover:p-2 w-72 sm:w-72 bg-zinc-900 rounded-lg overflow-hidden shadow-lg 
+                      duration-300 ease-in-out transform hover:translate-x-2 hover:translate-y-2
                      relative group"
             >
               <img
-                className="w-full h-40 object-cover transition-opacity duration-300 ease-in-out"
+                className="w-full rounded-md h-40 object-cover transition-opacity duration-300 ease-in-out"
                 src={d.backdrop_path || d.poster_path ? `https://image.tmdb.org/t/p/w500/${d.backdrop_path || d.poster_path}` : noimage}
                 alt={d.name || d.title || d.original_name || d.original_title || "Media image"}
                 loading="lazy"
               />
-              <div className="p-4">
+              <div className="p-1 ">
                 <h2 className="text-white text-lg font-bold mb-2 line-clamp-2">
                   {d.name || d.title || d.original_name || d.original_title}
                 </h2>
