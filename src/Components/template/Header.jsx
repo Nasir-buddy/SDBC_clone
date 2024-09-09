b8a4bc965f1607618678e949f25e60d44db301cc
nasir-buddy <syedali172000@gmail.com>
2024-09-09 18:08:23 +0530

    change button of and more

--- src/Components/template/Header.jsx
+++ src/Components/template/Header.jsx
@@ -18,7 +18,7 @@ const Header = ({ data }) => {
       </h1>
       <p className="text-white text-sm sm:text-base w-full sm:w-[90%] md:w-[80%] lg:w-[70%] mb-2">
         {data.overview.slice(0, 200)}...
-        <Link to={`/${data.media_type}/details/${data.id}`} className="text-blue-400 hover:text-blue-300 ml-1">
+        <Link to={`/${data.media_type}/details/${data.id}`} className="bg-gradient-to-bl from-slate-200 via-violet-500 to-zinc-400 bg-clip-text text-transparent">
           more
         </Link>
       </p>
@@ -38,9 +38,12 @@ const Header = ({ data }) => {
 
       <Link
         to={`/${data.media_type}/details/${data.id}/trailer`}
-        className="mt-2 sm:mt-4 bg-[#6556CD] hover:bg-[#5346bd] transition-colors duration-300 px-4 py-2 sm:px-6 sm:py-3 rounded text-white font-semibold text-sm sm:text-base"
-      >
-        Watch Trailer
+        className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-[#8E61F6] transition duration-300 ease-out border-2 border-[#8E61F6] rounded-full shadow-md group">
+        <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-[#8E61F6] group-hover:translate-x-0 ease">
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+        </span>
+        <span className="absolute flex items-center justify-center w-full h-full text-[#8E61F6] transition-all duration-300 transform group-hover:translate-x-full ease">Watch Trailer</span>
+        <span className="relative invisible">Button Text</span>
       </Link>
     </div>
   )
